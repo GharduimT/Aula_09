632fc9b26442221225bf3753fd1fdd3cef9643ac
GharduimT <gabriel.harduim.ti@gmail.com>
2025-05-08 19:21:48 -0300

    while enquanto for verdade

--- aula_09/exemplo_for_while.py
+++ aula_09/exemplo_for_while.py
@@ -17,3 +17,6 @@ while resposta != 'n': #enquando resposta for diferente de...
     nome =input('Informe o nome: ')
     lst_nomes.append(nome) #se parasse aqui haveria repetição continuamente
     
+    resposta = input('Quer continuar? [s/n]: ')
+
+print(f'Os nomes foram {lst_nomes}')
