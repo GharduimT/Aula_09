4a1da784a304e80d9196c0f519b02596b80023b7
GharduimT <gabriel.harduim.ti@gmail.com>
2025-05-08 19:09:34 -0300

    estrutura for

--- aula_09/exemplo_for_while.py
+++ aula_09/exemplo_for_while.py
@@ -1,1 +1,9 @@
 # Explicando for while
+
+lst_valores = [] #  lista vazia
+
+#estrutura For
+for i in range(5): #RANGE - intervalo numérico
+    num = int (input('Informe o número: '))
+    lst_valores.append(num) #  append
+print (f'Os números são{lst_valores}')
