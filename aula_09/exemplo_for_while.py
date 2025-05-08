d84389709ed5bb1b78d92ca1a6cab2cfa4553440
GharduimT <gabriel.harduim.ti@gmail.com>
2025-05-08 19:16:36 -0300

    while

--- aula_09/exemplo_for_while.py
+++ aula_09/exemplo_for_while.py
@@ -1,9 +1,19 @@
-# Explicando for while
+# # Explicando for while
 
-lst_valores = [] #  lista vazia
+# lst_valores = [] #  lista vazia
 
-#estrutura For
-for i in range(5): #RANGE - intervalo numérico
-    num = int (input('Informe o número: '))
-    lst_valores.append(num) #  append
-print (f'Os números são{lst_valores}')
+# #estrutura For
+# for i in range(5): #RANGE - intervalo numérico
+#     num = int (input('Informe o número: '))
+#     lst_valores.append(num) #  append
+# print (f'Os números são{lst_valores}')
+
+#While
+
+lst_nomes = []
+resposta = ''
+
+while resposta != 'n': #enquando resposta for diferente de...
+    nome =input('Informe o nome: ')
+    lst_nomes.append(nome) #se parasse aqui haveria repetição continuamente
+    
