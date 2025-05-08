ed47a564164e1cf3f6b22d3f594f8f981426b02f
GharduimT <gabriel.harduim.ti@gmail.com>
2025-05-08 19:39:04 -0300

    contador / loop infinito

--- aula_09/exemplo_for_while.py
+++ aula_09/exemplo_for_while.py
@@ -10,13 +10,32 @@
 
 #While
 
-lst_nomes = []
-resposta = ''
+# lst_nomes = []
+# resposta = ''
 
-while resposta != 'n': #enquando resposta for diferente de...
-    nome =input('Informe o nome: ')
-    lst_nomes.append(nome) #se parasse aqui haveria repetição continuamente
+# while resposta != 'n': #enquando resposta for diferente de...
+#     nome =input('Informe o nome: ')
+#     lst_nomes.append(nome) #se parasse aqui haveria repetição continuamente
     
-    resposta = input('Quer continuar? [s/n]: ') #como resposta não foi diferente de 'n' ele escreveu na tela o que guardou na lista
+#     resposta = input('Quer continuar? [s/n]: ') #como resposta não foi diferente de 'n' ele escreveu na tela o que guardou na lista
 
-print(f'Os nomes foram {lst_nomes}')
+# print(f'Os nomes foram {lst_nomes}')
+
+#contador
+lst_nomes = []
+contador = 0
+while contador < 5:
+     nome = input('informe o nome')
+    lst_nomes.append(nome)
+    contador += 1
+
+
+#loopinfinito
+lst_nomes2 = []
+while True:
+    nome = input('informe o nome')
+    lst_nomes2.append(nome)
+
+    resposta = input('Quer continuar? [s/n] ').lower()
+    if resposta == 'n':
+        break
